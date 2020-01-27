ea8a72e2811beeb1fecebb4e55f901d9215c7f86
Emma <emmagray183@gmail.com>
2020-01-27 15:00:01 +0000

    initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+//public class Runner {
+//
+//    public static void main(String[] args){
+//        Planet planet = new Planet("Mars", 908973);
+//        String name = planet.getName();
+//        int size = planet.getSize();
+//        String output = String.format("%s is %d miles long.");
+//        System.out.println(output);
+//    }
+//}
